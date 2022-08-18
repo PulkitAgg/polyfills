a063331a5a09f4fa2a5222288c5e622ff7a0a13c
Pulkit Aggarwal <pulkitagg1010@gmail.com>
2022-08-18 17:43:42 +0530

    Add new bind polyfill

--- bind.js
+++ bind.js
@@ -4,4 +4,24 @@ Function.prototype.myBind = function (obj, ...args) {
   return function (...newArgs) {
     func.apply(obj, [...args, ...newArgs]);
   };
-};
+}
+
+// If we don't want to use apply method
+Function.prototype.customBind = function (obj = {}, ...args) {
+  obj.func = this;
+  return function (...newArgs) {
+    obj.func(...args, ...newArgs);
+  };
+}
+
+// Way to check
+let name = { firstName: "Pulkit", lastName: "Aggarwal"}
+function fullName(first, second) {
+  console.log(this.firstName + " " + this.lastName + " " + first + " " + second);
+}
+
+fullName.myBind(name, "Hello", "World")();
+fullName.myBind(name, "Hello")("World");
+                               
+fullName.customBind(name, "Hello", "World")();
+fullName.customBind(name, "Hello")("World");
