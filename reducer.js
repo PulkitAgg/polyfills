ae9e1862486f6c779ad347cd90b2a361cbd1d978
Pulkit Aggarwal <pulkitagg1010@gmail.com>
2022-11-12 11:40:09 +0530

    Update function

--- reducer.js
+++ reducer.js
@@ -11,12 +11,13 @@ Array.prototype.MyReducer = function (fn, init) {
     startIndex = 1;
   }
   for (let count = startIndex; count < this.length; count++) {
-    current = fn(this[count], current);
+    current = fn(this[count], current, count, this);
   }
   return current;
 }
 
 //Way of calling MyReducer fucntion.
 let arr = [1,2,3,4];
-arr.MyReducer((a,b) => a+b); // Here 1 will be the initial value.
-arr.MyReducer((a,b) => a+b, 10); // Here 10 will be the initial value.
+let res1 = arr.MyReducer((a,b) => a+b); // Here 1 will be the initial value.
+let res2 = arr.MyReducer((a,b) => a+b, 10); // Here 10 will be the initial value.
+console.log("res1 ", res, " res2 ", res2);
